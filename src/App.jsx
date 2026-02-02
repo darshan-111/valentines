dc6b7f079a85f504034830186388c751b2e19b59
darshan sagare <darshansagare12@gmail.com>
2026-02-03 00:35:00 +0530

    added ayushi

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
 
       {!yesClicked ? (
         <>
-          <h1 className="title">Will you be my Valentine? 💘</h1>
+          <h1 className="title">Will you be my Valentine AYUSHI? 💘</h1>
 
           {/* <p className="no-text"></p> */}
 
